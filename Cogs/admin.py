69a47452882e83d9516af8df126aa18897976477
AalbatrossGuy <thexcelsiorisback@gmail.com>
2022-03-19 11:41:34 +0530

    Added ban/unban command

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -5,6 +5,8 @@ class Admin(interactions.Extension):
     def __init__(self, client):
         self.client = client
 
+    # Clear Command
+
     @interactions.extension_command(
         name="clear",
         description="Clear messages in chat.",
@@ -24,6 +26,8 @@ class Admin(interactions.Extension):
         await ctx.send(f"Purged {amount} messages!")
         await asyncio.sleep(2)
         await ctx.message.delete()
+
+    # Kick Command
 
     @interactions.extension_command(
         name="kick",
@@ -54,6 +58,8 @@ class Admin(interactions.Extension):
             await ctx.send("Oops! you don't have the required permission to run this command.")
 
 
+    # Ban Command
+
     @interactions.extension_command(
         name="ban",
         description="Bans a user from the server.",
@@ -81,6 +87,38 @@ class Admin(interactions.Extension):
             await ctx.send(f"Successfully Banned {member.mention} for `{reason}`")
         else:
             await ctx.send("Oops! you don't have the required permission to run this command.")
+
+
+    # Unban Command
+
+    @interactions.extension_command(
+        name="unban",
+        description="Unbans a user from the server.",
+        scope=903225083072495646,
+        options = [
+            interactions.Option(
+                name="member_id",
+                description="The member to unban from the server.",
+                type=interactions.OptionType.STRING,
+                required=True,
+            ),
+        ],
+    )
+    async def _unban_member(self, ctx, member_id: str) -> None:
+        member_id = int(member_id) if len(member_id) == 18 else 0
+        guild = await ctx.get_guild()
+        # print(f"Guild = {guild} & {guild.id}")
+        if member_id != 0:
+            print(guild)
+            _member = await self.client._http.get_member(guild_id=guild.id, member_id=member_id)
+            member = interactions.User(**_member)
+            if ctx.author.permissions & interactions.Permissions.BAN_MEMBERS:
+                await guild.remove_ban(user_id=member_id)
+                await ctx.send(f"Successfully Unbanned <@{member_id}>.")
+            else:
+                await ctx.send("Oops! you don't have the required permission to run this command.")
+        else:
+            await ctx.send("Please provide a valid Member ID to unban!")
 
 
 def setup(client):
