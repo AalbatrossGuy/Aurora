58cbda4a9e342558e34d92d37ec1637d4e96fe56
AalbatrossGuy <thexcelsiorisback@gmail.com>
2022-03-20 11:22:04 +0530

    Enabled logging system for AuroraBot

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import interactions, os, json, logging
+from datetime import datetime
 
 # CORE SETTINGS
 file = open('config.json')
 config = json.loads(file.read())
 TOKEN = config['Aurora']['TOKEN']
 client  = interactions.Client(token=TOKEN)
+
+# LOG SETTINGS
+logger = logging.getLogger("auroralog")
+logger.setLevel(logging.INFO)
 LOG_FILE = 'logs/info.log'
-logging.basicConfig(filename=LOG_FILE, filemode='w', level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
-logging.info('Bot Logged in Discord at - {time}')
+fileHandler = logging.FileHandler(LOG_FILE)
+fileHandler.setLevel(logging.INFO)
+LOG_FORMAT = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
+fileHandler.setFormatter(LOG_FORMAT)
+logger.addHandler(fileHandler)
+
+error_logger = logging.getLogger("error-logger")
+error_logger.setLevel(logging.DEBUG)
+ERROR_LOG_FILE = '././logs/errors.log'
+fileErrorHandler = logging.FileHandler(ERROR_LOG_FILE)
+fileErrorHandler.setLevel(logging.DEBUG)
+ERROR_LOG_FORMAT = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
+fileErrorHandler.setFormatter(ERROR_LOG_FORMAT)
+error_logger.addHandler(fileErrorHandler)
+
+
+# COMMANDS
 @client.command(
     name="ping",
     description="Shows the bot's latency.",
     scope=903225083072495646,
 )
 async def _bot_latency(ctx):
+    x = len(await client._http.get_application_commands(client.me.id, 903225083072495646))
+    print(x)
     connection_latency = client.latency
     embed = interactions.Embed(
         title = "Aurora's Latency",
@@ -36,8 +58,14 @@ async def _bot_latency(ctx):
     await ctx.send(embeds=embed)
 
 
+# LOAD COGS
 for filename in os.listdir('./Cogs'):
     if filename.endswith('.py'):
-        client.load(f"Cogs.{filename[:-3]}")
+        try:
+            client.load(f"Cogs.{filename[:-3]}")
+            logger.info(f"Loaded cogs.{filename[:-3]}")
+        except:
+            error_logger.error("Error occured while loading Cogs :-", exc_info=True)
 
+logger.info(f"Bot Logged in Discord at - {datetime.now().strftime('%H:%M:%S')}")
 client.start()
