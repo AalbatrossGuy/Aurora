487d798d13a9721f0e16723718873c031559c25a
AalbatrossGuy <thexcelsiorisback@gmail.com>
2022-03-20 11:22:59 +0530

    Defined custom function

--- customs/customs.py
+++ customs/customs.py
@@ -1,5 +1,5 @@
 # Start_info.py
-import subprocess
+import subprocess, interactions
 from datetime import datetime
 
 def version_info():
@@ -16,6 +16,71 @@ def version_info():
         else:
             pass
     return version, date
+
+def createEmbed(title: str, color: int, footer_text: str, footer_icon_url: str, thumbnail_url: str, description: str = None, author_name: str = None, author_icon_url: str = None) -> interactions.Embed:
+
+    if author_name & author_icon_url & description != None:
+            embed = interactions.Embed(
+                title = title,
+                description=description,
+                color=color,
+                footer=interactions.EmbedFooter(
+                    text=footer_text,
+                    icon_url=footer_icon_url,
+                ),
+                thumbnail=interactions.EmbedImageStruct(
+                    url=thumbnail_url,
+                )._json,
+                author=interactions.EmbedAuthor(
+                    name=author_name,
+                    icon_url=author_icon_url,
+                ),
+            )
+            return embed
+    elif author_name & author_icon_url != None:
+            embed = interactions.Embed(
+                title = title,
+                color=color,
+                footer=interactions.EmbedFooter(
+                    text=footer_text,
+                    icon_url=footer_icon_url,
+                ),
+                thumbnail=interactions.EmbedImageStruct(
+                    url=thumbnail_url,
+                )._json,
+                author=interactions.EmbedAuthor(
+                    name=author_name,
+                    icon_url=author_icon_url,
+                ),
+            )
+            return embed
+    elif description != None:
+            embed = interactions.Embed(
+                title = title,
+                description=description,
+                color=color,
+                footer=interactions.EmbedFooter(
+                    text=footer_text,
+                    icon_url=footer_icon_url,
+                ),
+                thumbnail=interactions.EmbedImageStruct(
+                    url=thumbnail_url,
+                )._json,
+            )
+            return embed
+    else:
+            embed = interactions.Embed(
+                title = title,
+                color=color,
+                footer=interactions.EmbedFooter(
+                    text=footer_text,
+                    icon_url=footer_icon_url,
+                ),
+                thumbnail=interactions.EmbedImageStruct(
+                    url=thumbnail_url,
+                )._json,
+            )
+            return embed
 
 
 if __name__ == '__main__':
