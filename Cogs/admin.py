7c1e11fc4028bedd23392de355c3cf7fd81b7457
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-06-24 14:25:59 +0530

    Migrated clear, kick & ban to discord.py v2.0

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -1,102 +1,63 @@
-import discord
+import discord, asyncio
 from discord.ext import commands
+from discord import app_commands
 
-
-#
 #  COMMANDS
 class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @app_commands.command(name="clear", description="Delete messages from a channel.")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    @app_commands.describe(
+        amount="The amount of messages to clear. The default is 10"
+    )
+    async def _clear_messages(self, interaction: discord.Interaction, amount: int = 10):
+        deleted_messages = await interaction.channel.purge(limit=amount)
+        await interaction.response.send_message(f"Deleted {len(deleted_messages)} Messages!")
+        await asyncio.sleep(3)
+        await interaction.delete_original_message()
 
-#
-#
-#     # Clear Command
-#     @interactions.extension_command(
-#         name="clear",
-#         description="Clear messages in chat.",
-#         options = [
-#         interactions.Option(
-#             name="amount",
-#             description="The number of messages you want to delete. By default it's 10.",
-#             type=interactions.OptionType.INTEGER,
-#             required=False,
-#         ),
-#      ],
-#     )
-#     async def _clear_messages(self, ctx, amount=10):
-#         try:
-#             channel = await ctx.get_channel()
-#             await channel.purge(amount=amount+1)
-#             await ctx.send(f"Purged {amount} messages!")
-#             await asyncio.sleep(2)
-#             await ctx.message.delete()
-#         except:
-#             error_logger.error("Error occured while responding to /clear :-", exc_info=True)
-#
-#
-#     # Kick Command
-#     @interactions.extension_command(
-#         name="kick",
-#         description="Kicks a user from the server.",
-#         options = [
-#             interactions.Option(
-#                 name="member",
-#                 description="The member to kick from the server.",
-#                 type=interactions.OptionType.USER,
-#                 required=True,
-#             ),
-#             interactions.Option(
-#                 name="reason",
-#                 description="The reason for kicking a member from the server.",
-#                 type=interactions.OptionType.STRING,
-#                 required=False,
-#             ),
-#         ],
-#     )
-#     async def _kick_member(self, ctx, member: interactions.Member, reason: str = "No reason provided."):
-#         try:
-#             guild = await ctx.get_guild()
-#             guild_id = guild.id
-#             if ctx.author.permissions & interactions.Permissions.KICK_MEMBERS:
-#                 await member.kick(guild_id=guild_id, reason=reason)
-#                 await ctx.send(f"Successfully kicked {member.mention} for `{reason}`")
-#             else:
-#                 await ctx.send("Oops! you don't have the required permission to run this command.")
-#         except:
-#             error_logger.error("Error occured while responding to /kick :-", exc_info=True)
-#
-#
-#     # Ban Command
-#     @interactions.extension_command(
-#         name="ban",
-#         description="Bans a user from the server.",
-#         options = [
-#             interactions.Option(
-#                 name="member",
-#                 description="The member to ban from the server.",
-#                 type=interactions.OptionType.USER,
-#                 required=True,
-#             ),
-#             interactions.Option(
-#                 name="reason",
-#                 description = "THe reason for banning a member from the server.",
-#                 type=interactions.OptionType.STRING,
-#                 required=False,
-#             ),
-#         ],
-#     )
-#     async def _ban_member(self, ctx, member: interactions.Member, reason: str = "No reason provided."):
-#         try:
-#             guild = await ctx.get_guild()
-#             guild_id = guild.id
-#             if ctx.author.permissions & interactions.Permissions.BAN_MEMBERS:
-#                 await member.ban(guild_id=guild_id, reason=reason)
-#                 await ctx.send(f"Successfully Banned {member.mention} for `{reason}`")
-#             else:
-#                 await ctx.send("Oops! you don't have the required permission to run this command.")
-#         except:
-#             error_logger.error("Error occured while responding to /ban :-", exc_info=True)
+    @app_commands.command(name="kick", description="Kick a Member from the guild.")
+    @app_commands.checks.has_permissions(kick_members=True)
+    @app_commands.describe(
+        member="The member to kick from the guild.",
+        reason="The reason for kicking from the guild."
+    )
+    async def _kick_member(self, interaction: discord.Interaction, member: discord.Member, reason: str = "N/A"):
+        embed = discord.Embed(title="<:kick:989811114567168051> Kicked", color=discord.Colour.dark_gold(),
+                              timestamp=interaction.created_at, description=f"You have been **kicked** from the server **{interaction.guild.name}** for the reason `{reason}`")
+        embed.set_footer(text="Auroras constantly change shape.",
+                         icon_url=interaction.user.display_avatar)
+        embed.set_thumbnail(
+            url="https://i.gifer.com/Bwtn.gif")
+        user_channel = await member.create_dm()
+        await user_channel.send(embed=embed)
+        await member.kick(reason=reason)
+        await interaction.response.send_message(f"Successfully kicked {member.mention} from the server!")
+        await asyncio.sleep(3)
+        await interaction.delete_original_message()
+
+    @app_commands.command(name="ban", description="Ban a Member from the guild.")
+    @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.describe(
+        member="The member to ban from the guild.",
+        reason="The reason for banning from the guild."
+    )
+    async def _ban_member(self, interaction: discord.Interaction, member: discord.Member, reason: str = "N/A"):
+        embed = discord.Embed(title="<:kick:989811114567168051> Banned", color=discord.Colour.dark_gold(),
+                              timestamp=interaction.created_at, description=f"You have been **banned** from the server **{interaction.guild.name}** for the reason `{reason}`")
+        embed.set_footer(text="The term ‘aurora borealis’ was coined in 1619",
+                         icon_url=interaction.user.display_avatar)
+        embed.set_thumbnail(
+            url="https://c.tenor.com/TbfChfHKkOUAAAAM/ban-button.gif")
+        user_channel = await member.create_dm()
+        await user_channel.send(embed=embed)
+        await member.ban(reason=reason)
+        await interaction.response.send_message(f"Successfully banned {member.mention} from the server!")
+        await asyncio.sleep(3)
+        await interaction.delete_original_message()
+
 #
 #
 #     # Unban Command
