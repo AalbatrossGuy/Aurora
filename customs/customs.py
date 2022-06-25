bbad5179136527160ebd9dab363052bd53a0d34f
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-06-25 20:04:38 +0530

    Updated date view format for version_info

--- customs/customs.py
+++ customs/customs.py
@@ -5,13 +5,13 @@ def version_info():
     version = 'No Data'
     date = 'No Data'
     git_log = subprocess.check_output(
-        ['git', 'log', '-n', '1', '--date=iso']).decode()
+        ['git', 'log', '-n', '1', '--date=format:"%d-%m-%Y %H:%M:%S"']).decode()
     for line in git_log.split('\n'):
         if line.startswith('commit'):
             version = line.split(' ')[1]
         elif line.startswith('Date'):
-            date = line[5:].strip()
-            date = date.replace(' +', '+').replace(' ', 'T')
+            date = line[5:].strip().replace('"', '')
+            # date = date.replace(' +', '+').replace(' ', 'T')
         else:
             pass
     return version, date
