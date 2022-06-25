613fa75f8fd0c7f7864897dd1940fcbc3f573bf1
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-06-25 20:03:47 +0530

    Added extensive logging

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import discord, logging, json, os, asyncio
+import discord.ext.commands
+from customs.customs import version_info
 from discord.ext import commands
 from typing import Optional, Literal
+from customs.log import AuroraLogger
+
+logger = AuroraLogger('AuroraInfoLog', 'logs/info.log')
+error_logger = AuroraLogger('AuroraErrorLog', 'logs/errors.log')
 
 file = open("config.json", "r")
 config = json.loads(file.read())
 TEST_GUILD = discord.Object(id=config["Aurora"]["TEST_GUILD"])
-handler = logging.FileHandler(filename="logs/aurora.log", encoding="utf-8", mode="w")
+handler = logging.FileHandler(filename="logs/info.log", encoding="utf-8", mode="w")
 
 client = commands.AutoShardedBot(command_prefix="!", case_insensitive=True, intents=discord.Intents.default())
 
@@ -14,30 +20,33 @@ client = commands.AutoShardedBot(command_prefix="!", case_insensitive=True, inte
 @commands.is_owner()
 async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object],
                spec: Optional[Literal["~", "*"]] = None) -> None:
-    if not guilds:
-        if spec == "~":
-            fmt = await ctx.bot.tree.sync(guild=ctx.guild)
-        elif spec == "*":
-            ctx.bot.tree.copy_global_to(guild=ctx.guild)
-            fmt = await ctx.bot.tree.sync(guild=ctx.guild)
-        else:
-            fmt = await ctx.bot.tree.sync()
+    try:
+        if not guilds:
+            if spec == "~":
+                fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "*":
+                ctx.bot.tree.copy_global_to(guild=ctx.guild)
+                fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+            else:
+                fmt = await ctx.bot.tree.sync()
 
-        await ctx.send(
-            f"Synced {len(fmt)} commands {'globally' if spec is None else 'to the current guild.'}"
-        )
-        return
+            await ctx.send(
+                f"Synced {len(fmt)} commands {'globally' if spec is None else 'to the current guild.'}"
+            )
+            return
 
-    fmt = 0
-    for guild in guilds:
-        try:
-            await ctx.bot.tree.sync(guild=guild)
-        except discord.HTTPException:
-            pass
-        else:
-            fmt += 1
+        fmt = 0
+        for guild in guilds:
+            try:
+                await ctx.bot.tree.sync(guild=guild)
+            except discord.HTTPException:
+                pass
+            else:
+                fmt += 1
 
-    await ctx.send(f"Synced the tree to {fmt}/{len(guilds)} guilds.")
+        await ctx.send(f"Synced the tree to {fmt}/{len(guilds)} guilds.")
+    except:
+        error_logger.error("Error occurred while executing !sync:- ", exc_info=True)
 
 
 @client.tree.command(name="ping", description="Shows the websocket latency and database latency.")
@@ -54,11 +63,32 @@ async def show_latency(interaction: discord.Interaction):
 
 
 async def main():
-    for filename in os.listdir('./Cogs'):
-        if filename.endswith('.py'):
-            await client.load_extension(f'Cogs.{filename[:-3]}')
-
-    await client.start(config["Aurora"]["TOKEN"])
+    cogs_count = 0
+    total_count = 0
+    try:
+        for filename in os.listdir('./Cogs'):
+            if filename.endswith('.py'):
+                total_count += 1
+                try:
+                    await client.load_extension(f'Cogs.{filename[:-3]}')
+                except discord.ext.commands.ExtensionFailed:
+                    cogs_count -= 1
+                    error_logger.error(f"Error occurred while loading cogs:- ", exc_info=True)
+                cogs_count += 1
+        logger.info(f"Successfully Loaded {cogs_count}/{total_count} Cogs.")
+        try:
+            await client.login(config["Aurora"]["TOKEN"])
+            bot_name = client.user.name
+            bot_discriminator = client.user.discriminator
+            logger.info(
+                f"Logged into discord as {bot_name}#{bot_discriminator}. Last commit ran on {version_info()[1]}")
+        except discord.LoginFailure or discord.HTTPException:
+            error_logger.critical("LOGIN TO DISCORD UNSUCCESSFUL :-", exc_info=True)
+            exit()
+        await client.connect()
+    except Exception as e:
+        if e != KeyboardInterrupt:
+            error_logger.error(f"Error occurred while starting the bot:- ", exc_info=True)
 
 
 client.tree.copy_global_to(guild=TEST_GUILD)
