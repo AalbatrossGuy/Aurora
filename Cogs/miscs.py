12c40faa6ad05870889b7f63eced85c75964c68f
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-07-10 07:38:52 +0530

    Member info command Complete

--- Cogs/miscs.py
+++ Cogs/miscs.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 from discord import app_commands
+from datetime import timezone
 from customs.log import AuroraLogger
 
 # LOGGER
@@ -40,6 +41,54 @@ class Miscs(commands.Cog):
             await interaction.response.send_message(embed=embed, view=buttons)
         except:
             error_logger.error("Error occurred while running avatar command:- ", exc_info=True)
+
+    @app_commands.command(name="meminfo", description="Shows information about the member")
+    @app_commands.describe(
+        member="The member whose info you wish to see."
+    )
+    async def _display_member_info(self, interaction: discord.Interaction, member: discord.Member = None):
+        try:
+            member = member or interaction.user
+            member_id = member.id
+            member_name = member.name
+            member_avatar_url = member.display_avatar
+            member_nickname = member.nick
+            is_bot = member.bot
+            member_top_role = member.top_role.mention
+            member_created_at = int(member.created_at.replace(tzinfo=timezone.utc).timestamp())
+            member_joined_at = int(member.joined_at.replace(tzinfo=timezone.utc).timestamp())
+            if member.display_avatar.is_animated() is True:
+                member_has_nitro = True
+            else:
+                member_has_nitro = False
+
+            embed = discord.Embed(title=f"{member_nickname if member_nickname is not None else member_name}'s Profile", description="<:damn:993688550119850015> *A nice profile you got kid!*", timestamp=interaction.created_at, color=member.color)
+            embed.set_footer(text="*Nitro is checked based on user's pfp", icon_url=interaction.user.display_avatar)
+            embed.set_thumbnail(url=member_avatar_url)
+            embed.add_field(name="Name", value=member_name)
+            embed.add_field(name="ID", value=member_id)
+            embed.add_field(name="Top Role", value=member_top_role)
+            embed.add_field(name="Joined At", value=f"<t:{member_joined_at}:F>\n(<t:{member_joined_at}:R>)")
+            embed.add_field(name="Created At", value=f"<t:{member_created_at}:F>\n(<t:{member_created_at}:R>)")
+            embed.add_field(name="Nitro?*", value=member_has_nitro)
+            permissions = dict(member.top_role.permissions)
+            lst = []
+            for keys, values in permissions.items():
+                if values:
+                    lst.append(f"• 2{keys}")
+                else:
+                    continue
+
+            before_len = len(lst)
+            if len(lst) > 5:
+                lst = lst[:5]
+                left = before_len - 5
+                lst.append(f"+{left} more...")
+            embed.add_field(name="Top Role Perms", value="\n".join(lst))
+
+            await interaction.response.send_message(embed=embed)
+        except:
+            error_logger.error("Error occurred while running member_info command:- ", exc_info=True)
 
 
 async def setup(client):
