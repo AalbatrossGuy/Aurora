8063a7a14e877330c7a37de0af44b3116b1e1c5b
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-07-10 08:11:05 +0530

    Added emojis for meminfo command and updated emoji for avatar buttons

--- Cogs/miscs.py
+++ Cogs/miscs.py
@@ -29,13 +29,13 @@ class Miscs(commands.Cog):
             avatar_webp = member.avatar.with_format('webp')
             buttons = discord.ui.View()
             buttons.add_item(
-                discord.ui.Button(label="PNG", style=discord.ButtonStyle.url, url=str(avatar_png), emoji="📸")
+                discord.ui.Button(label="PNG", style=discord.ButtonStyle.url, url=str(avatar_png), emoji=self.client.get_emoji(995519549057282108))
             )
             buttons.add_item(
-                discord.ui.Button(label="JPG", style=discord.ButtonStyle.url, url=str(avatar_jpg), emoji="📸")
+                discord.ui.Button(label="JPG", style=discord.ButtonStyle.url, url=str(avatar_jpg), emoji=self.client.get_emoji(995519549057282108))
             )
             buttons.add_item(
-                discord.ui.Button(label="WEBP", style=discord.ButtonStyle.url, url=str(avatar_webp), emoji="📸")
+                discord.ui.Button(label="WEBP", style=discord.ButtonStyle.url, url=str(avatar_webp), emoji=self.client.get_emoji(995519549057282108))
             )
             embed.set_image(url=base_avatar)
             await interaction.response.send_message(embed=embed, view=buttons)
@@ -65,12 +65,12 @@ class Miscs(commands.Cog):
             embed = discord.Embed(title=f"{member_nickname if member_nickname is not None else member_name}'s Profile", description="<:damn:993688550119850015> *A nice profile you got kid!*", timestamp=interaction.created_at, color=member.color)
             embed.set_footer(text="*Nitro is checked based on user's pfp", icon_url=interaction.user.display_avatar)
             embed.set_thumbnail(url=member_avatar_url)
-            embed.add_field(name="Name", value=member_name)
-            embed.add_field(name="ID", value=member_id)
-            embed.add_field(name="Top Role", value=member_top_role)
-            embed.add_field(name="Joined At", value=f"<t:{member_joined_at}:F>\n(<t:{member_joined_at}:R>)")
-            embed.add_field(name="Created At", value=f"<t:{member_created_at}:F>\n(<t:{member_created_at}:R>)")
-            embed.add_field(name="Nitro?*", value=member_has_nitro)
+            embed.add_field(name="<:lilgrux:995515614275829760> Name", value=member_name)
+            embed.add_field(name="<:lilgrux:995515614275829760> ID", value=member_id)
+            embed.add_field(name="<:top:995516251357073480> Top Role", value=member_top_role)
+            embed.add_field(name="<a:mcclock:995517509430161498> Joined At", value=f"<t:{member_joined_at}:F>\n(<t:{member_joined_at}:R>)")
+            embed.add_field(name="<a:mcclock:995517509430161498> Created At", value=f"<t:{member_created_at}:F>\n(<t:{member_created_at}:R>)")
+            embed.add_field(name="<a:nitrobaby:836902390766108694> Nitro?*", value=member_has_nitro)
             permissions = dict(member.top_role.permissions)
             lst = []
             for keys, values in permissions.items():
@@ -84,7 +84,7 @@ class Miscs(commands.Cog):
                 lst = lst[:5]
                 left = before_len - 5
                 lst.append(f"+{left} more...")
-            embed.add_field(name="Top Role Perms", value="\n".join(lst))
+            embed.add_field(name="<:roleicon:959329430428327936> Top Role Perms", value="\n".join(lst))
 
             await interaction.response.send_message(embed=embed)
         except:
