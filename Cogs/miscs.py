1221f6623407f90761b8e7e46a6f529250310e12
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2022-07-24 16:22:53 +0530

    Added Spotify Info Command

--- Cogs/miscs.py
+++ Cogs/miscs.py
@@ -1,3 +1,8 @@
+import datetime
+import math
+
+import dateutil.tz
+from dateutil import parser
 from discord.ext import commands
 import discord, json
 from discord import app_commands
@@ -39,7 +44,8 @@ class RightButton(discord.ui.View):
         embed = discord.Embed(title=f"<:okawaiikoto:998161222497226803> Random Waifu",
                               description=f"Name - {waifu_name}\nOriginal Name - {waifu_original_name}\nAppeared In - {waifu_appeared_in}",
                               url=waifu_site, timestamp=interaction.created_at, color=discord.Color.magenta())
-        embed.set_footer(text=f"The colors are about more than just looks • Page {self.counter}/10", icon_url=interaction.user.display_avatar)
+        embed.set_footer(text=f"The colors are about more than just looks • Page {self.counter}/10",
+                         icon_url=interaction.user.display_avatar)
         embed.set_image(url=waifu_picture)
         await interaction.message.edit(embed=embed)
 
@@ -148,17 +154,82 @@ class Miscs(commands.Cog):
             embed = discord.Embed(title=f"<:okawaiikoto:998161222497226803> Random Waifu",
                                   description=f"Name - {waifu_name}\nOriginal Name - {waifu_original_name}\nAppeared In - {waifu_appeared_in}",
                                   url=waifu_site, timestamp=interaction.created_at, color=discord.Color.magenta())
-            embed.set_footer(text="The colors are about more than just looks • Page 0/10", icon_url=interaction.user.display_avatar)
+            embed.set_footer(text="The colors are about more than just looks • Page 0/10",
+                             icon_url=interaction.user.display_avatar)
             embed.set_image(url=waifu_picture)
             view = RightButton()
             await interaction.followup.send(embed=embed, view=view)
         except:
             error_logger.error("An error occurred while running waifu command:- ", exc_info=True)
 
+    @app_commands.command(name="spotify", description="Shows the info of the current spotify song you're listening to")
+    @app_commands.describe(
+        member="The member who's spotify info you want."
+    )
+    async def show_spotify_info(self, interaction: discord.Interaction, member: discord.User = None):
+        try:
+            member = member or interaction.user
+            member = interaction.guild.get_member(member.id)
+            check_spotify = next((activity for activity in member.activities if isinstance(activity, discord.Spotify)),
+                                 None)
+
+            if check_spotify is None:
+                await interaction.response.send_message(
+                    f"<:nospotify:906890136996937729> {member.name} is not listening to any Spotify songs currently!")
+            else:
+                url = check_spotify.track_url
+                title = check_spotify.title if len(check_spotify.title) < 27 else f"{check_spotify.title[:27]}..."
+                artist_text = check_spotify.artist if len(
+                    check_spotify.artist) < 41 else f"{check_spotify.artist[:41]}..."
+                album = check_spotify.album if len(check_spotify.album) < 51 else f"{check_spotify.album[:41]}..."
+                duration_display = parser.parse(str(check_spotify.duration)).strftime('%M:%S')
+                duration = int(check_spotify.duration.total_seconds())
+                start_time = int(check_spotify.start.replace(tzinfo=timezone.utc).timestamp())
+                end_time = int(check_spotify.end.replace(tzinfo=timezone.utc).timestamp())
+                embed = discord.Embed(title=f"{member.nick if member.nick is not None else member.name}'s Spotify Song",
+                                      timestamp=interaction.created_at, color=discord.Color.brand_green(), url=url)
+                embed.set_image(url=check_spotify.album_cover_url)
+                embed.add_field(name="<:song:1000713276851769385> Song Title", value=title)
+                embed.add_field(name="<:mic:1000714771307450418> Artist(s)", value=artist_text)
+                embed.add_field(name="💽 Album", value=album, inline=False)
+                embed.add_field(name="⏳ Duration", value=duration_display)
+                embed.add_field(name="⏳ End Time", value=f"<t:{end_time}:R>")
+                embed.add_field(name="⏳ Start Time", value=f"<t:{start_time}:R>")
+                curr_time = int((datetime.datetime.utcnow().replace(tzinfo=None) - check_spotify.start.replace(
+                    tzinfo=None)).total_seconds())
+                curr_pos = int(curr_time / duration * 100)
+                blocks_to_display = int(curr_pos / 100 * 10)
+                bar_list = None
+                empty = "<:5499lb2g:1000696069363073035><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2881lb3g:1000696062073380864>"
+                full = "<:5988lbg:1000696379838046258><:3451lg:1000696066708099093><:3451lg:1000696066708099093><:3451lg:1000696066708099093><:3451lg:1000696066708099093><:3451lg:1000696066708099093><:3451lg:1000696066708099093><:3166lb4g:1000696064418009098>"
+                show_one_percent = "<:5988lbg:1000696379838046258><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2827l2g:1000696059154145342><:2881lb3g:1000696062073380864>"
+                if blocks_to_display == 0:
+                    bar_list = empty
+                if blocks_to_display == 10:
+                    bar_list = full
+                if blocks_to_display == 1:
+                    bar_list = show_one_percent
+                elif 0 < blocks_to_display < 10:
+                    bar_list = ["<:5988lbg:1000696379838046258>"] + ["<:3451lg:1000696066708099093>" for _ in
+                                                                     range(blocks_to_display-1)] + [
+                                   "<:2827l2g:1000696059154145342>" for _ in range(8 - blocks_to_display)] + [
+                                   "<:2881lb3g:1000696062073380864>"]
+                    bar_list = "".join(bar_list)
+
+                embed.add_field(name="Song Progress", value=bar_list)
+
+                await interaction.response.send_message(embed=embed)
+        except:
+            error_logger.error("An error occurred while running spotify command:- ", exc_info=True)
+
     @get_random_waifu.error
     async def on_get_random_waifu_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
         if isinstance(error, app_commands.CommandOnCooldown):
-            await interaction.response.send_message(embed=discord.Embed(title="<:hellno:871582891585437759> Rate Limited", description="You're on Cooldown! Please wait `5.0` seconds before running this command again.", color=discord.Color.blurple(), timestamp=interaction.created_at).set_footer(text="Dude, slow down...Take a chill pill"))
+            await interaction.response.send_message(
+                embed=discord.Embed(title="<:hellno:871582891585437759> Rate Limited",
+                                    description="You're on Cooldown! Please wait `5.0` seconds before running this command again.",
+                                    color=discord.Color.blurple(), timestamp=interaction.created_at).set_footer(
+                    text="Dude, slow down...Take a chill pill"))
 
 
 async def setup(client):
